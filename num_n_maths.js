059b27f614ac5a760c2ed18b6b8d3c3a653ee830
anujadak13 <91689110+anujadak13@users.noreply.github.com>
2024-09-21 11:37:00 +0530

    Add files via upload

---
+++ num_n_maths.js
@@ -0,0 +1,7 @@
+const number= new Number(1398.923653723);
+console.log(Math.floor(Math.random()*10)+1);// we are multiplying with 10 to get value > 0
+
+
+max=20
+min=10
+console.log(Math.floor(Math.random()*(max-min +1)+min))
