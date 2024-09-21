1758e896bf4a84da3015e4c410d9b0fa16d772fd
anujadak13 <91689110+anujadak13@users.noreply.github.com>
2024-09-21 11:37:24 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,9 @@
+const name="anuj kumar"
+const city= "mahoba";
+console.log(`My name is ${name} i am from ${city}`)
+
+const myname= new String("Anujkumardhuriya")
+
+console.log(myname.length)
+console.log(myname.substr(0,5))
+console.log(myname.split())
